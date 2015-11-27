7788142619e7717e518913c999e136b88d71685b
Angel <angel.arancibia@icloud.com>
2015-11-27 08:16:48 -0300

    some config changes in engine config

--- lib/system_notification/engine.rb
+++ lib/system_notification/engine.rb
@@ -8,25 +8,25 @@ module SystemNotification
     end
 
     # Included in the else from fix_migrations
-    initializer :append_migrations do |app|
-     unless app.root.to_s.match root.to_s
-       config.paths["db/migrate"].expanded.each do |expanded_path|
-         app.config.paths["db/migrate"] << expanded_path
-       end
-     end
-    end
-
-    # initializer :fix_migrations do |app|
-    #   if app.root.to_s.match root.to_s
-    #     task :set_annotation_options do
-    #       Rake::Task["engine:set_annotation_options"].invoke
-    #     end
-    #   else
-    #     config.paths["db/migrate"].expanded.each do |expanded_path|
-    #       app.config.paths["db/migrate"] << expanded_path
-    #     end
-    #   end
+    # initializer :append_migrations do |app|
+    #  unless app.root.to_s.match root.to_s
+    #    config.paths["db/migrate"].expanded.each do |expanded_path|
+    #      app.config.paths["db/migrate"] << expanded_path
+    #    end
+    #  end
     # end
+
+    initializer :fix_migrations do |app|
+      if app.root.to_s.match root.to_s
+        task :set_annotation_options do
+          Rake::Task["engine:set_annotation_options"].invoke
+        end
+      else
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      end
+    end
 
   end
 end
