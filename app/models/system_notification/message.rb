0ce1ae82df7332818d25e779a905ceaca6593db1
Angel <angel.arancibia@icloud.com>
2015-11-30 11:39:24 -0300

    Added callback feature to check for invariant

--- app/models/system_notification/message.rb
+++ app/models/system_notification/message.rb
@@ -1,12 +1,14 @@
 module SystemNotification
   class Message < ActiveRecord::Base
-    belongs_to :user, required: true
+    belongs_to :user, required: true, class_name: "::User"
 
     scope :unseen, -> { where(seen: false) }
     scope :not_dismissed, -> { where(dismiss: false) }
 
+    validates :level, presence: true
     enum level: [:debug, :info, :warning, :error ,:level0, :level1, :level2, :level3]
 
+    before_create :priority_handler
     # toggle the seen flag on
     def seen!
       self.seen = true
@@ -18,5 +20,25 @@ module SystemNotification
       self.dismiss = true
       self.save!
     end
+
+    protected
+
+    # auto priority handler
+    # on Creation of new notification,
+    # if there is another active one with lower or equal priority, dismiss it
+    # if there is another with greater priority, just set ourself as dismissed.
+    #
+    def priority_handler
+      self.class.where(user_id: self.user_id, dismiss: false).each do |m|
+        if m["level"] <= self["level"]
+          m.dismiss = true
+          m.save
+        else
+          # we got already a message with greater priority
+          self.dismiss = true
+        end
+      end
+    end
+
   end
 end
--- spec/models/system_notification/message_spec.rb
+++ spec/models/system_notification/message_spec.rb
@@ -6,8 +6,6 @@ module SystemNotification
       it 'Message is a valid factory' do
         expect(FactoryGirl.build(:system_notification_message).valid?).to be true
       end
-
-
     end
 
     describe 'seen/unseen' do
@@ -38,18 +36,53 @@ module SystemNotification
         create_list(:system_notification_message, 3, user: user)
       end
 
-      it {expect(user.notifications.take.dismiss?).to be false}
-      it {expect(user.notifications.not_dismissed.length).to be 3}
+      it {expect(user.notifications.first.dismiss?).to be true}
+      it {expect(user.notifications.last.dismiss?).to be false}
+      it {expect(user.notifications.not_dismissed.length).to eq 1}
       it {
-        n = user.notifications.take
+        n = user.notifications.not_dismissed.first
         n.dismiss!
         expect(n.dismiss?).to be true
       }
       it {
-        n = user.notifications.take
+        n = user.notifications[1]
         n.dismiss!
-        expect(user.notifications.not_dismissed.length).to be 2
+        expect(user.notifications.not_dismissed.length).to eq 1
       }
+    end
+
+    describe 'priority handler' do
+      let(:user) { FactoryGirl.create(:user) }
+
+      before :each do
+        create_list(:system_notification_message, 3, user: user, level: :info)
+      end
+
+      it {expect(user.notifications.not_dismissed.length).to eq 1}
+
+      describe 'create a equal priority message' do
+        before :each do
+          @notification = user.notifications.create(text:"text..", level: :info)
+        end
+        it {expect(@notification.dismiss?).to be false}
+        it {expect(user.notifications.not_dismissed.length).to eq 1}
+      end
+      describe 'create lower priority message' do
+        before :each do
+          @notification = user.notifications.create(text:"text..", level: :debug)
+        end
+        it {expect(@notification.dismiss?).to be true}
+        it {expect(user.notifications.not_dismissed.length).to eq 1}
+      end
+      describe 'create greater priority message' do
+        before :each do
+          @notification = user.notifications.create(text:"text..", level: :error)
+        end
+        it {expect(user.notifications.not_dismissed.first).to eq @notification}
+        it {expect(@notification.dismiss?).to be false}
+        it {expect(user.notifications.not_dismissed.length).to eq 1}
+      end
+
     end
   end
 end
